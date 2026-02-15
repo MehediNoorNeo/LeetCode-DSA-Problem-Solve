94c15f1c88f049846002d27eded977e7d2897781
Mehedi Noor <91518232+MehediNoorNeo@users.noreply.github.com>
2026-02-15 20:23:08 +0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (86.24%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,42 +11,54 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head = NULL;
-        ListNode* curr = NULL;
-
-        if(list1 == NULL) return list2;
-        if(list2 == NULL) return list1;
-        
+        // base case 
+        if(list1 == NULL || list2 == NULL){
+            return list1 == NULL ? list2 : list1;
+        }
         if(list1->val <= list2->val){
-            head = curr = list1;
-            list1 = list1->next;
-            curr->next = NULL;
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
         else{
-            head = curr = list2;
-            list2 = list2->next;
-            curr->next = NULL;
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-        while(list1 != NULL && list2 != NULL){
-            if(list1->val <= list2->val){
-                curr->next = list1;
-                curr = curr->next;
-                list1 = list1->next;
-                curr->next = NULL;
-            }
-            else{
-                curr->next = list2;
-                curr = curr->next;
-                list2 = list2->next;
-                curr->next = NULL;
-            }
-        }
-        if(list1 != NULL){
-            curr->next = list1;
-        }
-        if(list2 != NULL){
-            curr->next = list2;
-        }
-        return head;
+        // ListNode* head = NULL;
+        // ListNode* curr = NULL;
+
+        // if(list1 == NULL) return list2;
+        // if(list2 == NULL) return list1;
+
+        // if(list1->val <= list2->val){
+        //     head = curr = list1;
+        //     list1 = list1->next;
+        //     curr->next = NULL;
+        // }
+        // else{
+        //     head = curr = list2;
+        //     list2 = list2->next;
+        //     curr->next = NULL;
+        // }
+        // while(list1 != NULL && list2 != NULL){
+        //     if(list1->val <= list2->val){
+        //         curr->next = list1;
+        //         curr = curr->next;
+        //         list1 = list1->next;
+        //         curr->next = NULL;
+        //     }
+        //     else{
+        //         curr->next = list2;
+        //         curr = curr->next;
+        //         list2 = list2->next;
+        //         curr->next = NULL;
+        //     }
+        // }
+        // if(list1 != NULL){
+        //     curr->next = list1;
+        // }
+        // if(list2 != NULL){
+        //     curr->next = list2;
+        // }
+        // return head;
     }
 };
