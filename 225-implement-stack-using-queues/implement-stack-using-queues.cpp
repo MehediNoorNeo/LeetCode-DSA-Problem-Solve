f89be70bc23867287bff2d6c5854f323da2aaddd
Mehedi Noor <91518232+MehediNoorNeo@users.noreply.github.com>
2026-05-09 21:17:13 +0600

    Time: 0 ms (100.00%) | Memory: 9.5 MB (46.93%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,31 +1,50 @@
 class MyStack {
-    deque<int> dq;
+    queue<int> q1;
+    queue<int> q2;
 public:
     MyStack() {
         
     }
     
     void push(int x) {
-        dq.push_back(x);
+        if(q1.empty()){
+            q1.push(x);
+        }
+        else{
+            while(!q1.empty()){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            q1.push(x);
+            while(!q2.empty()){
+                q1.push(q2.front());
+                q2.pop();
+            }
+        }
     }
     
     int pop() {
-        if(dq.empty()){
+        if(q1.empty()){
             return -1;
         }
         else{
-            int ans = dq.back();
-            dq.pop_back();
+            int ans = q1.front();
+            q1.pop();
             return ans;
         }
     }
     
     int top() {
-        return dq.back();
+        if(q1.empty()){
+            return -1;
+        }
+        else{
+            return q1.front();
+        }
     }
     
     bool empty() {
-        return dq.empty();
+        return q1.empty();
     }
 };
 
